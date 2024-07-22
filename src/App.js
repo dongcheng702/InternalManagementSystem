e87599bbb3674993ce5a983ac17fb1c10bdb0e47
goku702 <47051985+dongcheng702@users.noreply.github.com>
2024-07-22 18:41:44 +0900

    2024年7月22日18:41:25
    一部分共同类css作成

--- src/App.js
+++ src/App.js
@@ -24,26 +24,26 @@
 
 // export default App;
 
-
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
-import MainLayout from './components/MainLayout';
-import EmployeeList from './components/EmployeeList';
-import WorkHourList from './components/WorkHourList';
-import CostManagement from './components/CostManagement';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import MainLayout from "./components/MainLayout";
+import EmployeeList from "./components/EmployeeList";
+import WorkHourList from "./components/WorkHourList";
+import CostManagement from "./components/CostManagement";
+import "./assets/css/global.css";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route element={<MainLayout />}>  
-          <Route path="/menu" element={<div>Menu Content</div>} /> {/* Load only the content */}
+        <Route element={<MainLayout />}>
+          <Route path="/menu" element={<div>Menu Content</div>} />{" "}
+          {/* Load only the content */}
           <Route path="/react/EmployeeList" element={<EmployeeList />} />
           <Route path="/react/WorkHourList" element={<WorkHourList />} />
           <Route path="/react/CostManagement" element={<CostManagement />} />
-          
         </Route>
       </Routes>
     </Router>
